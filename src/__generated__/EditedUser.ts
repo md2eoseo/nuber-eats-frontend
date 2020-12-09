493bc49526ad251a57234cb95e588b870a5310fb
md2eoseo <md2eoseo@gmail.com>
2020-12-09 17:45:44 +0900

    restaurants, categories query and category style

--- src/__generated__/EditedUser.ts
+++ src/__generated__/EditedUser.ts
@@ -10,4 +10,5 @@
 export interface EditedUser {
   __typename: "User";
   email: string;
+  verified: boolean;
 }
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,3 +1,79 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import {
+  restaurantsPageQuery,
+  restaurantsPageQueryVariables,
+} from "../../__generated__/restaurantsPageQuery";
 
-export const Restaurants = () => <h1>Restaurants</h1>;
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        address
+        category {
+          name
+        }
+        isPromoted
+      }
+    }
+  }
+`;
+
+export const Restaurants = () => {
+  const { data, loading } = useQuery<
+    restaurantsPageQuery,
+    restaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+      },
+    },
+  });
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex justify-center items-center">
+        <input
+          className="input w-3/12 rounded-md border-0"
+          type="Search"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="max-w-sm mx-auto py-8 flex justify-around">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex flex-col items-center cursor-pointer">
+                <div
+                  className="w-20 h-20 rounded-full bg-cover hover:bg-gray-100"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
