0000a3a7ffebc9a7990ee8245a9ace8e690ba25b
md2eoseo <md2eoseo@gmail.com>
2020-12-29 13:01:46 +0900

    added dish options input on AddDish page, show dishes on my-restaurant page

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -2,6 +2,7 @@ import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useParams } from "react-router-dom";
+import { Dish } from "../../components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import {
   myRestaurant,
@@ -35,7 +36,6 @@ export const MyRestaurant = () => {
     MY_RESTAURANT_QUERY,
     { variables: { input: { id: +id } } }
   );
-  console.log(data);
   return (
     <div>
       <Helmet>
@@ -65,7 +65,17 @@ export const MyRestaurant = () => {
         <div className="mt-10">
           {data?.myRestaurant.restaurant?.menu?.length === 0 ? (
             <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) : null}
+          ) : (
+            <div className="grid mt=16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.myRestaurant.restaurant?.menu?.map((dish) => (
+                <Dish
+                  name={dish.name}
+                  price={dish.price}
+                  description={dish.description}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
